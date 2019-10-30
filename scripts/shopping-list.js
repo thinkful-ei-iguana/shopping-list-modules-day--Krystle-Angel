a338de3f8ea7df368e208ffa89a64ccbdaac0a2a
Angel Alicea <ala31996@gmail.com>
2019-10-30 17:46:20 -0400

    need help with 6,7

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,5 +1,5 @@
 import store from './store.js';
-
+import item from './item.js';
 const generateItemElement = function (item) {
   let itemTitle = `<span class="shopping-item shopping-item__checked">${item.name}</span>`;
   if (!item.checked) {
@@ -63,15 +63,13 @@ const handleNewItemSubmit = function () {
   });
 };
 
-const toggleCheckedForListItem = function (id) {
-  const foundItem = store.items.find(item => item.id === id);
-  foundItem.checked = !foundItem.checked;
-};
 
 const handleItemCheckClicked = function () {
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
+    event.preventDefault();
     const id = getItemIdFromElement(event.currentTarget);
-    toggleCheckedForListItem(id);
+    store.findAndToggleChecked(id);
+    store.findAndDelete(id);
     render();
   });
 };
@@ -86,10 +84,10 @@ const getItemIdFromElement = function (item) {
  * Responsible for deleting a list item.
  * @param {string} id 
  */
-const deleteListItem = function (id) {
-  const index = store.items.findIndex(item => item.id === id);
-  store.items.splice(index, 1);
-};
+// const deleteListItem = function (id) {
+//   const index = store.items.findIndex(item => item.id === id);
+//   store.items.splice(index, 1);
+// };
 
 const handleDeleteItemClicked = function () {
   // like in `handleItemCheckClicked`, we use event delegation
@@ -103,17 +101,15 @@ const handleDeleteItemClicked = function () {
   });
 };
 
-const editListItemName = function (id, itemName) {
-  const item = store.items.find(item => item.id === id);
-  item.name = itemName;
-};
+// const editListItemName = function (id, itemName) {
+//   const item = store.items.find(item => item.id === id);
+//   item.name = itemName;
+// };
 
 /**
  * Toggles the store.hideCheckedItems property
  */
-const toggleCheckedItemsFilter = function () {
-  store.hideCheckedItems = !store.hideCheckedItems;
-};
+
 
 /**
  * Places an event listener on the checkbox
@@ -121,7 +117,7 @@ const toggleCheckedItemsFilter = function () {
  */
 const handleToggleFilterClick = function () {
   $('.js-filter-checked').click(() => {
-    toggleCheckedItemsFilter();
+    store.toggleCheckedItemsFilter();
     render();
   });
 };
@@ -132,6 +128,7 @@ const handleEditShoppingItemSubmit = function () {
     const id = getItemIdFromElement(event.currentTarget);
     const itemName = $(event.currentTarget).find('.shopping-item').val();
     editListItemName(id, itemName);
+    store.findAndUpdateName(id);
     render();
   });
 };
@@ -150,4 +147,3 @@ export default {
   bindEventListeners
 };
 
-import item from './item.js';
