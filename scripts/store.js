ea9f81f84ca4e070da005eea4c8eab03ac587294
Angel Alicea <ala31996@gmail.com>
2019-10-30 17:17:03 -0400

    blah

--- scripts/store.js
+++ scripts/store.js
@@ -1,10 +1,55 @@
+import item from './item.js';
 let items = [];
 let hideCheckedItems = false;
 
-export default {
-    items,hideCheckedItems,
-    hideCheckedItems: hideCheckedItems,
+const findById = function (id){
+    store.items.find(ele => {
+        return store.items.ele;
+    })
 };
 
-import item from './item.js';
+const addItem = function (name) {
+    try {
+        item.validateName(name);
+        const newItem = item.create(name)
+        this.items.push(newItem);        
+    } catch (error) {
+        console.log(`Cannot add item: ${error.message}`);
+
+    }
+};
+
+const findAndUpdateName = function (id,newName){
+    try{
+        item.validateName(newName);
+        item.findById();
+    } catch (error) {
+        console.log(`cannot update name ${error.message}`);
+    }
+
+
+}
+
+const findAndToggleChecked = function (id){
+    this.findById();
+    render();
+}
+
+const findAndDelete = function(id){
+    const index = findById(id);
+    items.splice(index,1);
+  };
+
+
+
+export default {
+    items, 
+    hideCheckedItems, 
+    addItem, 
+    findById, 
+    findAndDelete, 
+    findAndToggleChecked, 
+    findAndUpdateName
+};
+
 
